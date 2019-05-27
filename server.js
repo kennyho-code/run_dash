42e2448be891ba64ca66e5b6572645588888712b
kennyho <kennyho.code@gmail.com>
2019-05-27 09:07:36 -0400

    server connect please

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Serve static files....
 app.use(express.static(__dirname + '/dist/run-dash'));
 
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/run-dash/index.html'));
+});
 
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/run-dash/index.html'));
-})
-
-app.listen(process.env.PORT || 5000);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
