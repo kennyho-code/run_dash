1744aab53a7ba70a375038dc0b76482a953df08d
kennyho <kennyho.code@gmail.com>
2019-05-27 12:07:41 -0400

    layout

---
+++ src/app/layout/navigation/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  @Output()
+  sidenavToggle = new EventEmitter<void>();
+
+
+
+  ngOnInit() {
+  }
+
+  onToggleSidenav(){
+    this.sidenavToggle.emit();
+  }
+
+}
---
+++ src/app/layout/navigation/side-navigation/side-navigation.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navigation',
+  templateUrl: './side-navigation.component.html',
+  styleUrls: ['./side-navigation.component.css']
+})
+export class SideNavigationComponent implements OnInit {
+
+  @Output()
+  closeSidenav = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClose(){
+    this.closeSidenav.emit();
+  }
+
+}
