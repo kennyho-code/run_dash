b6bb81010413d50c1fc1993ae04a5dc1679da319
kennyho <kennyho.code@gmail.com>
2019-05-30 09:01:13 -0400

    add to prod api

--- src/app/core/services/global.services.ts
+++ src/app/core/services/global.services.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalServices{
-    // public baseUrl = 'https://shrouded-shore-78872.herokuapp.com/'
-    public baseUrl = 'http://127.0.0.1:8000/';
+    public baseUrl = 'https://shrouded-shore-78872.herokuapp.com/'
+    // public baseUrl = 'http://127.0.0.1:8000/';
 }
