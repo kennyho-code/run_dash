d69950a7732bfadf09215e39c60e445a075c3eed
kennyho <kennyho.code@gmail.com>
2019-06-01 16:46:22 -0400

    add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AnalyticsComponent } from './analytics/analytics.component'
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'analytics', component: AnalyticsComponent}
 ];
